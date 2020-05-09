f28c17956bb4294546a617d13016306d3f223c71
Bryan Mundie <bryan.mundie@htt.io>
2020-05-09 11:44:51 -0400

    fix routing bug

--- app/router.js
+++ app/router.js
@@ -7,5 +7,5 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('minimum-calc-stuff', { path: '/' });
+  this.route('minimum-number-of-runs', { path: '/' });
 });
