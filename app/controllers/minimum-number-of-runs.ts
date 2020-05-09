eea39149a83c1bcc30fe21eb078227112d0c2ffb
Bryan Mundie <bryan.mundie@htt.io>
2020-05-09 10:39:11 -0400

    add calculating

---
+++ app/controllers/minimum-number-of-runs.ts
@@ -0,0 +1,55 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import {action} from "@ember/object";
+
+export default class MinimumNumberOfRuns extends Controller.extend({
+  // anything which *must* be merged to prototype here
+}) {
+  @tracked width = 1;
+  @tracked length = 1;
+
+  get area() {
+    return this.width * this.length;
+  }
+
+  get twoFtBoundryExclusionDeterminant() {
+    return this.area - 16 * (this.length + this.width) + 64;
+  }
+
+  get twoFtExclusionApplies() {
+    return this.twoFtBoundryExclusionDeterminant > 0;
+  }
+  
+  get NminZ() {
+    return 2 * Math.sqrt(this.area);
+  }
+
+  get Smax() {
+    return Math.floor((this.width - 4) * Math.sqrt(2));
+  }
+
+  get Zmax() {
+    return 2 * this.Smax - 19;
+  }
+
+  get MinRuns() {
+    let minRuns = Math.ceil(this.NminZ / this.Zmax);
+    return minRuns > 2 ? minRuns : 2;
+  }
+
+  @action 
+  setLength(length: string) {
+    this.length = Number(length);
+  }
+  @action
+  setWidth(width: string) {
+    this.width = Number(width);
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
+declare module '@ember/controller' {
+  interface Registry {
+    'minimum-number-of-runs': MinimumNumberOfRuns;
+  }
+}
